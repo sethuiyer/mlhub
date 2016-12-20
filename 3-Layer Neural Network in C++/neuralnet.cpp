5408af3e46fd7c13709ad564d6d069c4f4f9545c
Sethu Iyer <sethuiyer95@gmail.com>
2016-12-20 17:53:50 +0530

    Added costfunction

--- 3-Layer Neural Network in C++/neuralnet.cpp
+++ 3-Layer Neural Network in C++/neuralnet.cpp
@@ -1,3 +1,7 @@
+//Neural network in C++
+// Author: Sethu Iyer 
+//Sigmoid is used because the sigmoid function assumes minimal structure and reflects our general state of ignorance about the underlying model.
+
 #include <iostream>
 #include <armadillo>
 
@@ -25,15 +29,42 @@ public:
         W1.randu(2,3);
         W2.randu(3,1);
     }
+
+    //helper functions
+    static double sigmoid(double x)
+{
+  return x / (1 + abs(x));
+}
+static double sigmoidPrime(double x)
+{
+  double f = sigmoid(x);
+  return f * (1-f);
+}
+
+//forwarding function.
 arma::mat forward(arma::mat X)
 {
     z2 = X * W1;
     a2 = z2;
-    a2.transform([](double x) {return x / (1 + abs(x)); });
+    a2.transform(*sigmoid);
     z3 = a2 * W2;
     a3 = z3;
-    a3.transform([] (double x) { return x / (1+abs(x));});
+    a3.transform(*sigmoid);
     return a3;
+}
+
+double costFunction(mat X,mat y)
+{
+  mat yHat = forward(X);
+
+  double cost = 0;
+  for(int i = 0; i < outputLayerSize; i++)
+  {
+    cost =  cost + (yHat[i] - y[i])*(yHat[i] - y[i]);
+  }
+  cost = cost / 2;
+  return cost;
+
 }
 
 };
@@ -44,8 +75,6 @@ int main()
     mat X = {{0.3,1},{0.5,0.2},{1,0.4}};
     mat y = {0.75,0.82,0.93};
   Neural_Network nn;
-  mat yHat = nn.forward(X);
-  cout<<yHat[0]<<" "<<y[0]<<endl;
-  cout<<yHat[1]<<" "<<y[1]<<endl;
-  cout<<yHat[2]<<" "<<y[2]<<endl;
+  cout<<nn.costFunction(X,y);
+  return 0;
 }
