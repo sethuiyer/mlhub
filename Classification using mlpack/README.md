3c85c6d06cd04e8d571dcecf115027d94b451317
Sethu Iyer <sethuiyer95@gmail.com>
2016-12-28 21:24:16 +0530

    Added new proj

---
+++ Classification using mlpack/README.md
@@ -0,0 +1,4 @@
+#Email Classification using mlpack
+
+* Run `bash train.sh` after building mlpack
+* You can then load the same model by `-m model.txt` next time.
---
+++ Classification using mlpack/train.sh
@@ -0,0 +1,2 @@
+mlpack_logistic_regression -t data/train.txt -T data/test.txt -M model.txt -o output.txt
+echo 'Model has been trained. Output stored in output.txt'
